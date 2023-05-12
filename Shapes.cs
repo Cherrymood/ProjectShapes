a343892572c45cb851629630291a4d966dcc2f7a
Valeriia <olxlerasport@gmail.com>
2023-05-12 11:56:47 -0700

    Created class Shape

---
+++ Shapes.cs
@@ -0,0 +1,14 @@
+namespace DrawingShaps
+{
+    public class Shape
+    {
+        private double _length;
+        private double _width;
+
+        public void Shapes(double length, double width)
+        {
+            _length = length;
+            _width = width;
+        }
+    }
+}
