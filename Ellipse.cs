c7d25a4dc8c6beb01bade4c625e0bd6215c8be73
Valeriia <olxlerasport@gmail.com>
2023-05-15 15:26:10 -0700

    Added CalculateArea()

--- Ellipse.cs
+++ Ellipse.cs
@@ -17,8 +17,10 @@ namespace DrawingShaps
             return 2 * Math.PI*(Math.Sqrt((Math.Pow(length, 2) + Math.Pow(width, 2)/2)));
         }
 
-
-
+        public override double CalculateArea()
+        {
+            return Math.PI * length * width;
+        }
 
     }
 }
