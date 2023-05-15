6094872e459254c50fbb84c6242619ea8171ac07
Valeriia <olxlerasport@gmail.com>
2023-05-15 15:43:45 -0700

    Added debugging and prittier code

--- Program.cs
+++ Program.cs
@@ -5,18 +5,12 @@
         static void Main(string[]args)
         {
             var shape = new Shape(20, 10);
-            shape.CalculateArea();
-            shape.CalculatePerimeter();
             shape.PrintTostring();
 
             var rectangle = new Rectangle(25, 60);
-            rectangle.CalculateArea();
-            rectangle.CalculatePerimeter();
             rectangle.PrintTostring();
 
             var ellipse = new Ellipse(22.4, 66.9);
-            ellipse.CalculateArea();
-            ellipse.CalculatePerimeter();
             ellipse.PrintTostring();
 
 
