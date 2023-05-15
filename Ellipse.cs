0f11ac003fc1fed50b5741f71b07dd9f7af99e90
Valeriia <olxlerasport@gmail.com>
2023-05-15 15:24:24 -0700

    Added PrintTostring and CalculatePerimeter methods

--- Ellipse.cs
+++ Ellipse.cs
@@ -7,5 +7,18 @@ namespace DrawingShaps
         {
             
         }
+        public override void PrintTostring()
+        {
+            Console.WriteLine("I am a ellipse with measures {0} SemiMajorAxes and {1} SemiMajorAxes", length, width);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI*(Math.Sqrt((Math.Pow(length, 2) + Math.Pow(width, 2)/2)));
+        }
+
+
+
+
     }
 }
