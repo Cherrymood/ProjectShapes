f426520db9e3dbf96ea61d50ec8b32b231f71b6d
Valeriia <olxlerasport@gmail.com>
2023-05-15 17:40:31 -0700

    1

--- Ellipse.cs
+++ Ellipse.cs
@@ -14,12 +14,12 @@ namespace DrawingShaps
 
         public override double CalculatePerimeter()
         {
-            return 2 * Math.PI*(Math.Sqrt((Math.Pow(length, 2) + Math.Pow(width, 2)/2)));
+            return Math.Round(2 * Math.PI*(Math.Sqrt((Math.Pow(length, 2) + Math.Pow(width, 2)/2))));
         }
 
         public override double CalculateArea()
         {
-            return Math.PI * length * width;
+            return Math.Round(Math.PI * length * width);
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             var shape = new Shape(20, 10, "shape");
             shape.PrintTostring();
 
-            var rectangle = new Rectangle(25, 60, "rectangle");
+            var rectangle = new Rectangle(15.8, 60, "rectangle");
             rectangle.PrintTostring();
 
             var ellipse = new Ellipse(22.4, 66.9, "ellipse");
