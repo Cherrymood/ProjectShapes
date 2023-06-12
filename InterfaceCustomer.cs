b95698c1c0625dc8372142ad1f2bcb4b101b28aa
Valeriia <olxlerasport@gmail.com>
2023-06-12 15:52:34 -0700

    Created interface for clustomers

---
+++ InterfaceCustomer.cs
@@ -0,0 +1,13 @@
+namespace ArcadeGameCafe
+{
+    interface ICustomer
+    {
+        public void takeMenue();
+        public void makeOrder();
+        public void waitOrder();
+        public void takeOrder();
+        public void giveMoney();
+        public void goOut();
+        public void enter();
+    }
+}
