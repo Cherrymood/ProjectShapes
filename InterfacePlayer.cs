fde7106e891b5c2be9f7bc6c55f568400e9401bc
Valeriia <olxlerasport@gmail.com>
2023-06-12 15:49:55 -0700

    Created players intergace

---
+++ InterfacePlayer.cs
@@ -0,0 +1,12 @@
+namespace ArcadeGameCafe
+{
+    interface IPlayer
+    {
+        public void giveMenue();
+        public void takeOrder();
+        public void prepareOrder();
+        public void giveOrder();
+        public void takeMoney();
+        public void cleanUp();
+    }
+}
