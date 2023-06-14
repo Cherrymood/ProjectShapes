3b756710a40323f1530e0fb7b0603219230967b5
Valeriia <olxlerasport@gmail.com>
2023-06-14 12:29:49 -0700

    Added positions to Menue class

---
+++ Menue.cs
@@ -0,0 +1,19 @@
+namespace ArcadeGameCafe
+{
+    public class Menue
+    {
+       public string greenTea = "Geen tea";
+       public string blackTea = "Black tea";
+       public string bubbleTea = "Bubble tea";
+
+       public string americanoCoffee = "Americano";
+       public string latteCoffee =  "Latte";
+       public string cakesFood = "Cake";
+       public string wafflesFood = "Waffle";
+       public string icecreamFood = "Ice-Cream";
+
+       public string vanillaTopping = "Vanilla Topping";
+       public string chocolateTopping = "Chocolate Topping";
+       public string caramelTopping = "Caramel Topping";
+    }
+}
