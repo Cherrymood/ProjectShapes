e8c61008827c96568fb74d04e82d5831eb4ffed0
Valeriia <olxlerasport@gmail.com>
2023-06-16 11:58:41 -0700

    Added preparingTime

--- Menue.cs
+++ Menue.cs
@@ -3,7 +3,8 @@ namespace ArcadeGameCafe
     public class Menue
     {
         private Dictionary<string, double> _menue;
-        
+        private Dictionary<string, double> _preparingtime;
+
 
         public Dictionary<string, double> menue
         {
@@ -24,6 +25,27 @@ namespace ArcadeGameCafe
 
         };}
             get {return _menue;}
+        }
+
+        public Dictionary<string, double> preparingTime
+        {
+            set {_menue = new Dictionary<string, double>()
+        {
+            {"Americano", 1.30},
+            {"Latte", 3.00},
+            {"Raf", 4.00},
+            {"Green Tea", 1.00},
+            {"Black Tea", 1.00},
+            {"Bubble Tea", 3.00},
+            {"Cake", 0.40},
+            {"Ice-cream", 3.00},
+            {"Waffles", 9.00},
+            {"Chocolate topings", 0.30},
+            {"Caramel topings", 0.30},
+            {"Vanilla topings", 0.30}
+
+        };}
+            get {return _preparingtime;}
         }
     }
 }
