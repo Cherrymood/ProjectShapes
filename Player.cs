d3c46656af9e245e341fae1286daaa66944d076f
Valeriia <olxlerasport@gmail.com>
2023-06-16 12:10:52 -0700

    Added Preorder method to Player

--- Player.cs
+++ Player.cs
@@ -3,6 +3,7 @@ namespace ArcadeGameCafe
     public class Player : IPlayer
     {
         private bool IsOrderRady = false;
+        private bool IsgiveOrder;
         
         public void TakeOrder(string _key)
         {
@@ -10,9 +11,22 @@ namespace ArcadeGameCafe
         }
         public bool PrepareOrder(string _key)
         {
+            var newmenue = new Menue();
+            var waitingtime = newmenue.preparingTime.GetValueOrDefault(_key);
+            waitingtime *= 60;
+            for (double i = 0; i <= waitingtime; i ++)
+            {
+                i++;
+            }
+            
             return IsOrderRady = true;
         }
-        public void GiveOrderToCustomer();
+        public void GiveOrderToCustomer(bool IsOrderRady)
+        {
+
+        }
+
+
         public double TakeMoney(string _key);
         public void CleanUp();
     }
